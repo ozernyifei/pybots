de22c1c23687eb6ece2327ffde8dc2f560c881a2
Toadles <ozernyifei@outlook.com>
2023-02-16 22:23:17 +0300

    add new_mood which is rly working

--- mood_bot.py
+++ mood_bot.py
@@ -1,55 +1,106 @@
 import pyodbc
 import telebot
+import datetime
 from telebot import types
 
-# Подключение к базе данных
-conn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=database_name;UID=username;PWD=password')
-cursor = conn.cursor()
+server_name = 'localhost'  # Имя вашего сервера
+database_name = 'MoodBotDB'  # Название вашей базы данных
+username = 'botbotname'  # Ваше имя пользователя для доступа к базе данных
+password = 'password'  # Ваш пароль для доступа к базе данных
+
+# Строка подключения
+connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server_name};DATABASE={database_name};UID={username};PWD={password}'
+
+# Создаем соединение
+cnxn = pyodbc.connect(connection_string)
+
+cursor = cnxn.cursor()
 
 # Создание бота
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('6290464531:AAHyfuLMg5eMZMe348O8TxkVXQ7vhEdE1vA')
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
+
 def start(message):
-    markup = types.ReplyKeyboardMarkup()
+    
+    markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
     good_button = types.KeyboardButton('Хорошее')
     average_button = types.KeyboardButton('Среднее')
     bad_button = types.KeyboardButton('Плохое')
     markup.add(good_button, average_button, bad_button)
     bot.send_message(message.chat.id, 'Какое у вас сегодня настроение?', reply_markup=markup)
 
+@bot.message_handler(func=lambda message: message.text in ['Хорошее', 'Среднее', 'Плохое'])
+def add_mood_to_db(message):
+    user_id = message.chat.id
+    now = datetime.datetime.now()  # Получаем текущее время
+    last_mood_time = get_last_mood_time(message.chat.id)  # Получаем время последнего добавления настроения
+
+    # Проверяем, прошло ли более 24 часов с момента последнего добавления настроения
+    if last_mood_time is None or now - last_mood_time >= datetime.timedelta(hours=24):
+        # Выполняем запрос на вставку в базу данных
+        cursor.execute("INSERT INTO mood (user_id, mood, time) VALUES (?, ?, ?)",
+                       (user_id, message, now))
+        cnxn.commit()  # Сохраняем изменения в базе данных
+        return True
+    else:
+        return False
+
 # Обработчик кнопок
 @bot.message_handler(func=lambda message: message.text in ['Хорошее', 'Среднее', 'Плохое'])
 def mood_handler(message):
-    # Проверяем, была ли запись настроения от пользователя за последние 24 часа
-    # query = "SELECT * FROM mood WHERE user_id = ? AND time > DATEADD(hour, -24, GETDATE())"
-    # cursor.execute(query, message.chat.id)
-    # row = cursor.fetchone()
+    # Получаем из базы данных последнюю запись настроения пользователя
+    query = "SELECT TOP 1 * FROM mood WHERE user_id = ? ORDER BY time DESC"
+    cursor.execute(query, message.chat.id)
+    row = cursor.fetchone()
 
-    # # Если запись была, обновляем ее, иначе добавляем новую запись
-    # if row:
-    #     query = "UPDATE mood SET mood = ?, time = GETDATE() WHERE id = ?"
-    #     cursor.execute(query, message.text, row.id
-    # else:
-    #     query = "INSERT INTO mood (user_id, mood, time) VALUES (?, ?, GETDATE())"
-    #     cursor.execute(query, message.chat.id, message.text)
+    if row:
+        # Проверяем, прошло ли уже 24 часа с момента последней записи настроения
+        last_time = row.time
+        time_diff = (datetime.datetime.now() - last_time).total_seconds() // 3600
+        if time_diff < 24:
+            # Если прошло меньше 24 часов, сообщаем пользователю, что он уже сегодня сообщил свое настроение
+            bot.send_message(message.chat.id, f'Вы уже сообщили свое настроение сегодня. Следующая запись будет доступна через {24 - time_diff} часа(ов).')
+        else:
+            # Если прошло больше 24 часов, даем пользователю возможность обновить свое настроение
+            mood_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            mood_keyboard.add(types.KeyboardButton('Хорошее'))
+            mood_keyboard.add(types.KeyboardButton('Среднее'))
+            mood_keyboard.add(types.KeyboardButton('Плохое'))
+            bot.send_message(message.chat.id, 'Какое у вас настроение?', reply_markup=mood_keyboard)
+    else:
+        # Если у пользователя еще нет записей настроения, предлагаем ему сообщить свое настроение
+        mood_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        mood_keyboard.add(types.KeyboardButton('Хорошее'))
+        mood_keyboard.add(types.KeyboardButton('Среднее'))
+        mood_keyboard.add(types.KeyboardButton('Плохое'))
+        bot.send_message(message.chat.id, 'Какое у вас настроение?', reply_markup=mood_keyboard)
+    
+def get_last_mood_time(user_id):
+    cursor.execute("SELECT TOP 1 time FROM mood WHERE user_id = ? ORDER BY time DESC", (user_id))
+    row = cursor.fetchone()
+    if row is not None:
+        return row[0]
+    else:
+        return None
+        
 
-    # conn.commit()
-    bot.send_message(message.chat.id, 'Ваше настроение сохранено')
+
 
 # Обработчик команды /mood
 @bot.message_handler(commands=['mood'])
 def mood_report(message):
+    user_id = message.chat.id
     query = "SELECT * FROM mood WHERE user_id = ? AND time > DATEADD(day, -7, GETDATE())"
-    cursor.execute(query, message.chat.id)
+    cursor.execute(query, user_id)
     rows = cursor.fetchall()
 
     if rows:
         mood_report = '\n'.join(f'{row.time}: {row.mood}' for row in rows)
-        bot.send_message(message.chat.id, f'Ваше настроение за последние 7 дней:\n{mood_report}')
+        bot.send_message(user_id, f'Ваше настроение за последние 7 дней:\n{mood_report}')
     else:
-        bot.send_message(message.chat.id, 'У вас пока нет записей о настроении')
+        bot.send_message(user_id, 'У вас пока нет записей о настроении')
 
 # Запуск бота
 bot.polling(none_stop= True)
---
+++ new_mood_bot.py
@@ -0,0 +1,149 @@
+import pyodbc
+import telebot
+import datetime
+from telebot import types
+
+
+server_name = 'localhost'  # Имя вашего сервера
+database_name = 'MoodBotDB'  # Название вашей базы данных
+username = 'botbotname'  # Ваше имя пользователя для доступа к базе данных
+password = 'password'  # Ваш пароль для доступа к базе данных
+
+# Строка подключения
+connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server_name};DATABASE={database_name};UID={username};PWD={password}'
+
+# Создаем соединение
+cnxn = pyodbc.connect(connection_string)
+
+cursor = cnxn.cursor()
+
+# Создание бота
+bot = telebot.TeleBot('6290464531:AAHyfuLMg5eMZMe348O8TxkVXQ7vhEdE1vA')
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    username = message.from_user.username
+
+    # Проверяем, есть ли пользователь в базе данных
+    cursor.execute("SELECT id FROM users WHERE id=?", user_id)
+    row = cursor.fetchone()
+
+    if not row:
+        # Добавляем пользователя в базу данных
+        cursor.execute("INSERT INTO users (id, username) VALUES (?, ?)", user_id, username)
+        cnxn.commit()
+
+        bot.reply_to(message, "Привет! Я буду записывать твои настроения. Используй кнопки, чтобы отправить свое текущее настроение.", reply_markup=keyboard)
+
+    else:
+        bot.reply_to(message, "С возвращением! Используй кнопки, чтобы отправить свое текущее настроение.", reply_markup=keyboard)
+
+
+# Define inline keyboard
+keyboard = types.ReplyKeyboardMarkup(row_width=3)
+good_button = types.KeyboardButton(text="Good")
+okay_button = types.KeyboardButton(text="Okay")
+bad_button = types.KeyboardButton(text="Bad")
+keyboard.add(good_button, okay_button, bad_button)
+
+# Отправляем сообщение с кнопками пользователю
+
+
+# # Define callback query handler
+# @bot.callback_query_handler(func=lambda call: True)
+# def handle_query(call):
+#     user_id = call.message.chat.id
+#     today = datetime.datetime.now().date()
+#     last_mood_time = get_last_mood_time(user_id)
+#     if last_mood_time and last_mood_time.date() == today:
+#         bot.answer_callback_query(callback_query_id=call.id, text="You have already submitted your mood today!")
+#     else:
+#         if call.data == 'good':
+#             mood = 'good'
+#         elif call.data == 'okay':
+#             mood = 'okay'
+#         elif call.data == 'bad':
+#             mood = 'bad'
+#         add_mood_to_db(user_id, mood)
+#         bot.answer_callback_query(callback_query_id=call.id, text=f"Your {mood} mood has been saved!")
+@bot.message_handler(func=lambda message: message.text in ['Good', 'Ok', 'Bad'])
+def handle_query(message):
+    user_id = message.chat.id
+
+
+    # Получаем время последнего добавления настроения пользователя
+    last_mood_time = get_last_mood_time(user_id)
+
+    # Если пользователь пытается добавить настроение раньше, чем через 24 часа, выдаем сообщение
+    if last_mood_time and datetime.datetime.now() - last_mood_time < datetime.timedelta(days=1):
+        bot.send_message(user_id, "Вы уже добавляли настроение сегодня. Попробуйте еще раз завтра!")
+        return
+
+    # Добавляем настроение пользователя в базу данных
+    add_mood_to_db(user_id, message.text)
+
+    # Отправляем сообщение об успешном добавлении настроения
+    bot.send_message(user_id, "Ваше настроение успешно добавлено!")
+
+# Define message handler
+@bot.message_handler(commands=['mood'])
+def send_mood(message):
+    user_id = message.chat.id
+    last_mood_time = get_last_mood_time(user_id)
+    if not last_mood_time:
+        bot.send_message(user_id, "You haven't submitted your mood yet.")
+    else:
+        mood_data = get_mood_data(user_id, last_mood_time)
+        if not mood_data:
+            bot.send_message(user_id, "You haven't submitted your mood yet.")
+        else:
+            mood_list = [f"{data[0]}: {data[1]}" for data in mood_data]
+            mood_history = '\n'.join(mood_list)
+            bot.send_message(user_id, f"Your mood history for the past 7 days:\n{mood_history}")
+
+# Add mood data to SQL database
+def add_mood_to_db(user_id, mood):
+    now = datetime.datetime.now()
+    cursor.execute("INSERT INTO mood (user_id, mood, time) VALUES (?, ?, ?)", user_id, mood, now)
+    cnxn.commit()
+
+# Get the datetime of the last mood submission
+def get_last_mood_time(user_id):
+    cursor.execute("SELECT TOP 1 time FROM mood WHERE user_id = ? ORDER BY time DESC", user_id)
+    row = cursor.fetchone()
+    if row:
+        return row[0]
+
+# Get mood data for the past 7 days
+def get_mood_data(user_id):
+    conn = pyodbc.connect('Driver={SQL Server};'
+                          'Server=your_server_name;'
+                          'Database=your_database_name;'
+                          'Trusted_Connection=yes;')
+    cursor = conn.cursor()
+
+    # Получаем данные о настроении пользователя за последние 7 дней
+    end_date = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    start_date = end_date - datetime.timedelta(days=7)
+    cursor.execute(f"SELECT CAST(date AS date), mood FROM mood WHERE user_id = ? AND date >= ? AND date <= ?", (user_id, start_date, end_date))
+    rows = cursor.fetchall()
+
+    mood_data = []
+    for row in rows:
+        mood_data.append({'date': row[0], 'mood': row[1]})
+
+
+    return mood_data
+
+
+    
+
+        
+
+
+
+
+# Запуск бота
+bot.polling(none_stop= True)
